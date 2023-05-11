99cff3f18c03ff54013c78e2fd98a17d137155ae
Артур Печенюк <velort777@yandex.ru>
2023-05-12 01:04:02 +0300

    little test v3

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -19,8 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apitest/', include('api.urls', namespace='api')),
-
+    path('api/', include('api.urls', namespace='api')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
